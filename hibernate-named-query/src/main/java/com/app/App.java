158d91dd2973211ec368ad7ef60db347f92a3cb6
Sourabh Gaikwad <sourabhg9009@gmail.com>
2023-12-15 12:32:24 +0530

    new commit

---
+++ hibernate-named-query/src/main/java/com/app/App.java
@@ -0,0 +1,33 @@
+package com.app;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+	public static void main(String[] args) {
+		SessionFactory factory = 
+				new Configuration().
+				configure("hibernate.cfg.xml").
+				addAnnotatedClass(Employee.class)
+				.buildSessionFactory();
+		Session session = factory.openSession();
+		// Hibernate Named Query
+		TypedQuery<Employee> query = 
+				session.getNamedQuery("findEmployeeByName");
+		query.setParameter("name", "nikita");
+		List<Employee> employees = query.getResultList(); 
+		Iterator<Employee> itr = employees.iterator();
+		while (itr.hasNext()) {
+			Employee e = itr.next();
+			System.out.println(e);
+		}
+		// session.getTransaction().commit();
+		session.close();
+	}
+}
